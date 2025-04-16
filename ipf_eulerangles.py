97d4d016616c03f2831199f987412ba3e7ca9d5b
Anil Kunwar <anilkunwar318@yahoo.com>
2025-04-17 00:29:07 +0200

    Update ipf_eulerangles.py

--- ipf_eulerangles.py
+++ ipf_eulerangles.py
@@ -1,3 +1,5 @@
+import os
+os.environ["STREAMLIT_DEPRECATION_SHOW_PYPLOT_GLOBAL_USE"] = "False"
 import streamlit as st
 st.set_option('deprecation.showPyplotGlobalUse', False)
 #import streamlit as st
